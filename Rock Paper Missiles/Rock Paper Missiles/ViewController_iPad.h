242d9fc649efe7c0158297bdd584e8811f36ae7c
mguniverse <dannyperski@gmail.com>
2015-11-24 15:25:01 -0700

    Minor changes.

---
+++ Rock Paper Missiles/Rock Paper Missiles/ViewController_iPad.h
@@ -0,0 +1,49 @@
+//
+//  ViewController_iPad.h
+//  Rock Paper Missiles
+//
+//  Created by Danny Perski on 9/13/15.
+//  Copyright (c) 2015 Danny Perski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+@interface ViewController_iPad : UIViewController {
+    AVAudioPlayer *buttonSound;
+    
+    BOOL animating;
+    
+    int buttonRadius;
+    int soundClip;
+    
+    int currentMenu;
+    int currentSubMenu;
+    float animationSpeed;
+    
+    IBOutlet UILabel *bannerLabel;
+    
+    IBOutlet UIButton *playButton;
+    IBOutlet UIButton *aboutButton;
+    
+    IBOutlet UIButton *backButton;
+    
+    IBOutlet UIView *opponentInfo1;
+    
+    IBOutlet UIView *supremacyView;
+    IBOutlet UIView *rushView;
+    IBOutlet UIView *aboutView;
+    IBOutlet UIView *backView;
+    IBOutlet UIView *beginView;
+    
+    IBOutlet UIView *dimView;
+    IBOutlet UIView *errorView;
+    IBOutlet UITextView *errorText;
+    IBOutlet UILabel *errorLabel;
+    
+    UIImage *sourceImage;
+    NSTimer *cinematicTimer;
+}
+
+@end
